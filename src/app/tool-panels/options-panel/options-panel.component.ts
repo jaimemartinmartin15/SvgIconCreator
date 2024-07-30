50b786bfdc9c6e49d4a9d287960df3d822c47d16
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2024-07-30 17:50:10 +0200

    Refactor options panel to select previous colors.

--- src/app/tool-panels/options-panel/options-panel.component.ts
+++ src/app/tool-panels/options-panel/options-panel.component.ts
@@ -1,7 +1,10 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
 import { CollapsibleModule, ColorPickerComponent } from '@jaimemartinmartin15/jei-devkit-angular-shared';
+import { Subject, takeUntil } from 'rxjs';
+import { ShapePainter } from '../../painters/shape.painter';
+import { Shape } from '../../shape';
 
 @Component({
   selector: 'app-options-panel',
@@ -10,13 +13,41 @@ import { CollapsibleModule, ColorPickerComponent } from '@jaimemartinmartin15/je
   templateUrl: './options-panel.component.html',
   styleUrls: ['./options-panel.component.scss'],
 })
-export class OptionsPanelComponent {
+export class OptionsPanelComponent implements OnChanges {
+  public Shape = Shape;
   public colorFormControlName: 'stroke' | 'fill' = 'stroke';
 
+  private otherShapeIsSelected$ = new Subject<void>();
+  private lastUsedStroke: string = '#000000'; // black
+  private lastUsedFill: string = '#FFFFFF'; // white
+
   @Input()
-  public shapeForm: FormGroup;
+  public shapePainter: ShapePainter;
+
+  public ngOnChanges(changes: SimpleChanges): void {
+    if (changes['shapePainter']) {
+      // if shapePainter has changed
+
+      // end subscription of previous shape form
+      this.otherShapeIsSelected$.next();
+
+      // listen to changes for next shape
+      (this.shapePainter.options.controls['stroke'] as FormControl).valueChanges
+        .pipe(takeUntil(this.otherShapeIsSelected$))
+        .subscribe((v) => (this.lastUsedStroke = v));
+      (this.shapePainter.options.controls['fill'] as FormControl).valueChanges
+        .pipe(takeUntil(this.otherShapeIsSelected$))
+        .subscribe((v) => (this.lastUsedFill = v));
+    }
+
+    if (changes['shapePainter'] && !this.shapePainter.isShapeSelected()) {
+      // if it changes the shapePainter and it is not selected (it is a new one), set last used colors in colorpicker
+      this.shapePainter.options.controls['stroke'].setValue(this.lastUsedStroke);
+      this.shapePainter.options.controls['fill'].setValue(this.lastUsedFill);
+    }
+  }
 
   public getColorFormControl(): FormControl {
-    return this.shapeForm.controls[this.colorFormControlName] as FormControl;
+    return this.shapePainter.options.controls[this.colorFormControlName] as FormControl;
   }
 }
