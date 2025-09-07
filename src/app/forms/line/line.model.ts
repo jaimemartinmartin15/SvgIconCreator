0cd05a8a8cd03901ea4288085ceb836f888d9ebf
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-09-07 17:10:18 +0200

    Remove all files. Update workflow.

--- src/app/forms/line/line.model.ts
+++
@@ -1,8 +0,0 @@
-import { SharedModel } from '../shared.model';
-
-export interface LineModel extends SharedModel {
-  x1: number;
-  y1: number;
-  x2: number;
-  y2: number;
-}
--- src/app/forms/text/text.model.ts
+++
@@ -1,8 +0,0 @@
-import { SharedModel } from '../shared.model';
-
-export interface TextModel extends SharedModel {
-  x: number;
-  y: number;
-  text: string;
-  fontSize: number;
-}
