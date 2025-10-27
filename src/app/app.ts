1e509836a6b4d78610c870b39a16655fc8242c75
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2025-10-27 20:48:52 +0100

    Avoid closing app without saving changes.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ShapeListService } from '../services/shape-list.service';
 
 @Component({
   selector: 'app-root',
@@ -15,4 +16,15 @@ import { RouterOutlet } from '@angular/router';
     `,
   ],
 })
-export class App {}
+export class App {
+  constructor(private readonly shapeListService: ShapeListService) {}
+
+  @HostListener('window:beforeunload', ['$event'])
+  onBeforeunload(event: BeforeUnloadEvent) {
+    if (this.shapeListService.shapeList.length > 0) {
+      // Message of the pop up cannot be edited
+      event.preventDefault();
+      event.returnValue = ''; // For legacy browsers
+    }
+  }
+}
