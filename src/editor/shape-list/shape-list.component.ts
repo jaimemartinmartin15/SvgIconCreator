37743996c68e42866e3c44c1b4626bd25877857c
Jaime Martín Martín <jaimemartinmartin15@gmail.com>
2026-07-16 19:04:27 +0200

    Refactor shape list component.

--- src/editor/shape-list/shape-list.component.ts
+++ src/editor/shape-list/shape-list.component.ts
@@ -1,29 +1,48 @@
-import { CdkDrag, CdkDragDrop, CdkDragPlaceholder, CdkDropList, moveItemInArray } from '@angular/cdk/drag-drop';
-import { Component, EventEmitter, Output, ChangeDetectionStrategy } from '@angular/core';
+import { CdkDrag, CdkDragDrop, CdkDropList, CdkDropListGroup, moveItemInArray } from '@angular/cdk/drag-drop';
+import { AfterViewInit, ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
 import { ShapeListService } from '../../services/shape-list.service';
-import { ShapeHost } from '../../shapes/shape-host';
-import { ShapeElementComponent } from './shape-element/shape-element.component';
+import { GroupHost } from '../../shapes/group-host';
+import { PlusSvgComponent } from '../../svg-output/plus.component';
+import { ShapeGroupComponent } from './shape-group/shape-group.component';
 
 @Component({
   selector: 'app-shape-list',
   templateUrl: './shape-list.component.html',
   styleUrls: ['./shape-list.component.scss'],
   changeDetection: ChangeDetectionStrategy.Eager,
-  imports: [CdkDrag, CdkDragPlaceholder, CdkDropList, ShapeElementComponent],
+  imports: [CdkDrag, CdkDropListGroup, CdkDropList, ShapeGroupComponent, PlusSvgComponent],
 })
-export class ShapeListComponent {
+export class ShapeListComponent implements AfterViewInit {
   @Output()
+  // TODO check this emitter
   public openBindingsDialog = new EventEmitter<void>();
+
+  public get groups(): GroupHost[] {
+    return this.shapeListService.groupsList;
+  }
 
   public constructor(private readonly shapeListService: ShapeListService) {}
 
-  public get shapes(): ShapeHost[] {
-    return this.shapeListService.shapeList;
+  public ngAfterViewInit(): void {
+    // create always a new group on app launch
+    this.newGroup();
   }
 
-  public onReorderingShapes(event: CdkDragDrop<string[]>) {
-    this.shapes.forEach((shapeHost) => shapeHost.removeFromCanvas());
-    moveItemInArray(this.shapes, event.previousIndex, event.currentIndex);
-    this.shapes.forEach((shapeHost) => shapeHost.addToCanvas());
+  //#region header buttons
+  public newGroup(): void {
+    const g = this.shapeListService.createNewGroup();
+    this.shapeListService.selectedGroup = g;
+  }
+
+  public duplicateShape(): void {
+    // TODO
+    throw new Error('Method not implemented.');
+  }
+  //#endregion
+
+  public onReorderingGroups(event: CdkDragDrop<GroupHost[]>) {
+    this.groups.forEach((group) => group.removeFromCanvas());
+    moveItemInArray(this.groups, event.previousIndex, event.currentIndex);
+    this.groups.forEach((group) => group.addToCanvas());
   }
 }
